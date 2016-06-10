c8edb0f1b1e01a75413766a39d6d6e349c2fa79d
SStrissel <stephan.strissel@hs-worms.de>
2016-06-10 18:58:00 +0200

    kommentar

--- Controller/ControllerKriterium.cs
+++ Controller/ControllerKriterium.cs
@@ -8,7 +8,7 @@ namespace NWAT_SS_165
 {
 
 
-    // Kommentar
+    // gdfgdfggdfgfd
     class KriteriumController : Controller
     {
         public void aendern(Kriterium objekt, int ProjektID = 0, int ProduktID = 0)
