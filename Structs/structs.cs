a2db0550a12bfae484b3865f3fdd1e96e616da27
Stephan Strissel <stephan.strissel@hs-worms.de>
2016-06-15 23:05:06 +0200

    struct

---
+++ Structs/structs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NWAT_SS_165
+{
+    public struct listModel
+    {
+        int id;
+        string name;
+    }
+}
